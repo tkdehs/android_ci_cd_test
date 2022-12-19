a2068b76b984e31d410a530e29206ac58cccf3e8
SangDon <tkdehs0454@naver.com>
2022-12-19 17:17:49 +0900

    test
    test

--- app/src/main/java/com/movingroot/cicd/MainActivity.kt
+++ app/src/main/java/com/movingroot/cicd/MainActivity.kt
@@ -32,6 +32,7 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+        // test
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
